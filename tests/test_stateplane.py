0351e915ea42ad46f10ea4664052186de30ad938
fitnr <fitnr@fakeisthenewreal>
2020-07-02 10:57:42 -0400

    tweak tests

--- tests/test_stateplane.py
+++ tests/test_stateplane.py
@@ -10,12 +10,13 @@
 
 from __future__ import unicode_literals
 import unittest
-import stateplane
-from stateplane import cli
 from random import uniform
+import stateplane
+
 
 def rand():
     return uniform(-116.593781, -82.316437), uniform(32.287133, 42.293564)
+
 
 class TestCase(unittest.TestCase):
 
@@ -30,12 +31,12 @@ class TestCase(unittest.TestCase):
         result = stateplane.from_lonlat(-75.2, 40.2)
         fixture = (817080.8169336573, 99364.28495057777)
         for a in zip(result, fixture):
-            self.assertAlmostEqual(*a)
+            self.assertAlmostEqual(*a, 5)
 
         result = stateplane.from_latlon(40.2, -75.2)
         fixture = (817080.8169336573, 99364.28495057777)
         for a in zip(result, fixture):
-            self.assertAlmostEqual(*a)
+            self.assertAlmostEqual(*a, 5)
 
     def test_tolonlat(self):
         x, y = stateplane.to_lonlat(817080.8169336573, 99364.28495057777, epsg='32129')
@@ -52,19 +53,19 @@ class TestCase(unittest.TestCase):
             stateplane.to_lonlat(817080.8169336573, 99364.28495057777)
 
     def test_get_co(self):
-        assert stateplane.stateplane._get_co('01001') == '26930'
-        assert stateplane.stateplane._get_co('72123') == '32161'
+        self.assertEqual(stateplane.stateplane._get_co('01001'), '26930')
+        self.assertEqual(stateplane.stateplane._get_co('72123'), '32161')
 
     def test_identify_with_countyfp(self):
-        assert 'NY_LI' == stateplane.identify(None, None, fmt='short', statefp='36', countyfp='005')
-        assert 'NY_LI' == stateplane.identify(None, None, fmt='short', countyfp='36005')
+        self.assertEqual('NY_LI', stateplane.identify(None, None, fmt='short', statefp='36', countyfp='005'))
+        self.assertEqual('NY_LI', stateplane.identify(None, None, fmt='short', countyfp='36005'))
 
     def test_identify(self):
-        assert 'NY_LI' == stateplane.identify(-73.604107, 40.750638, fmt='short')
-        assert 'NC' == stateplane.identify(-80.1, 36.2, fmt='short')
-        assert '3200' == stateplane.identify(-80.1, 36.2, fmt='fips')
-        assert stateplane.identify(-75.2, 40.2, fmt='short') == 'PA_S'
-        assert stateplane.identify(-75.2, 40.2) == '32129'
+        self.assertEqual('NY_LI', stateplane.identify(-73.604107, 40.750638, fmt='short'))
+        self.assertEqual('NC', stateplane.identify(-80.1, 36.2, fmt='short'))
+        self.assertEqual('3200', stateplane.identify(-80.1, 36.2, fmt='fips'))
+        self.assertEqual(stateplane.identify(-75.2, 40.2, fmt='short'), 'PA_S')
+        self.assertEqual(stateplane.identify(-75.2, 40.2), '32129')
 
         fixture = (
             "+proj=lcc +lat_1=36.16666666666666 +lat_2=34.33333333333334 "
@@ -74,7 +75,6 @@ class TestCase(unittest.TestCase):
         self.assertEqual(stateplane.identify(-80.1, 36.2, fmt='proj4').strip(), fixture)
 
         self.assertEqual(stateplane.identify(-80.1, 36.2, fmt='FOOBAR'), '32119')
-
 
     def test_inverting(self):
         for _ in range(500):
@@ -86,7 +86,6 @@ class TestCase(unittest.TestCase):
             self.assertAlmostEqual(lat, y)
 
     def test_statefp(self):
-        assert '26943' == stateplane.identify(-121.986361, 37.524384999999995, statefp='06')
+        self.assertEqual('26943', stateplane.identify(-121.986361, 37.524384999999995, statefp='06'))
         self.assertEqual('26957', stateplane.identify(0, 0, statefp='10'))
         self.assertRaises(ValueError, stateplane.identify, -121, 35.7, statefp='KK')
-
