d251a4956c22a37d65a047850bda11924d11a391
fitnr <fitnr@fakeisthenewreal>
2022-02-06 14:33:30 -0500

    use setup.cfg
    move coveragerc to pyproject

--- setup.py
+++ setup.py
@@ -1,60 +1,8 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 # This file is part of stateplane.
 # https://github.com/fitnr/stateplane
-
 # Licensed under the GPLv3 license:
 # http://http://opensource.org/licenses/GPL-3.0
 # Copyright (c) 2015, Neil Freeman <contact@fakeisthenewreal.org>
 
-from setuptools import setup, find_packages
-
-try:
-    readme = open('README.md').read()
-except IOError:
-    readme = ''
-
-setup(
-    name='stateplane',
-    version='0.4.0',
-    description='Convert between state plane projections and long/lat',
-    long_description=readme,
-    long_description_content_type='text/markdown',
-    keywords='gis usa projection',
-    author='Neil Freeman',
-    author_email='contact@fakeisthenewreal.org',
-    url='https://github.com/fitnr/stateplane',
-    license='GPLv3',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Operating System :: Unix',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Operating System :: OS Independent',
-    ],
-    packages=find_packages(),
-    include_package_data=True,
-
-    package_data={
-        'stateplane': ['data/*.zip', 'data/*.csv']
-    },
-
-    install_requires=[
-        'pyproj>=2.4.2',
-        'shapely',
-        'GDAL',
-    ],
-    test_suite='tests',
-
-    entry_points={
-        'console_scripts': [
-            'stateplane=stateplane.cli:main',
-        ],
-    },
-    zip_safe=False,
-)
+from setuptools import setup
+setup()
--- src/stateplane/__init__.py
+++ src/stateplane/__init__.py
@@ -10,7 +10,7 @@
 
 from . import stateplane
 
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 
 identify = stateplane.identify
 
