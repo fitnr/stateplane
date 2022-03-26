ee4e80591fbdfa541d8672ddc58f12f07b32d940
fitnr <fitnr@fakeisthenewreal>
2022-03-26 10:42:46 -0400

    consolidate project metadata to pyproject.toml

--- setup.py
+++
@@ -1,8 +0,0 @@
-# This file is part of stateplane.
-# https://github.com/fitnr/stateplane
-# Licensed under the GPLv3 license:
-# http://http://opensource.org/licenses/GPL-3.0
-# Copyright (c) 2015, Neil Freeman <contact@fakeisthenewreal.org>
-
-from setuptools import setup
-setup()
